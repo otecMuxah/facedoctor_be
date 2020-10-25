3069df8ae1136ccc3c558b146bf2300757e0993f
mishaMalyavin <mykhailo.maliavin@teaminternational.com>
2020-10-25 21:56:32 +0200

    init

--- facedoctor/settings.py
+++ facedoctor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from distutils.command.config import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRT KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = 'cz$070jieqi6t1gixqfkak4f*cf9mask2ojn*3lcfm9vo7_j@g'
 
 ALLOWED_HOSTS = [
     'facedoctor.herokuapp.com'
@@ -128,3 +125,4 @@ STATIC_URL = '/static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200"
 ]
+
